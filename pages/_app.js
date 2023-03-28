baae1bbe00f8dbac7befdd896dc979704413279b
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2023-03-28 10:11:33 +0800

    added localstorage theme switcher

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "@/scss/layout/index.scss";
 import "@fontsource/nunito-sans/400.css";
 import "@fontsource/nunito-sans/600.css";
 import "@fontsource/nunito-sans/800.css";
-import React, { useState } from "react";
+import React, { useState, useEffect, use } from "react";
 import { Provider } from "react-redux";
 import store from "@/redux/store";
 import { ThemeContext } from "@/context/theme-context";
@@ -16,6 +16,18 @@ import "../scss/components/country-card-loader.scss";
 
 export default function App({ Component }) {
   const [darkTheme, setDarkTheme] = useState(false);
+
+  useEffect(() => {
+    const darkTheme = JSON.parse(localStorage.getItem("darkTheme"));
+
+    if (darkTheme) {
+      setDarkTheme(darkTheme);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("darkTheme", darkTheme);
+  }, [darkTheme]);
 
   return (
     <>
